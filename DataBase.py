60e0c81d97127980e7da2c374cfab87f441b2298
jianghao <256481788@163.com>
2016-05-19 17:08:42 +0800

    add getdata function

--- DataBase.py
+++ DataBase.py
@@ -1,8 +1,28 @@
+import tushare as ts
+import os
+import pandas as pd
+
 class DataBase:
+    def get_share_history_data(self,code):
+        if not self.has_share_history_local_data(code):
+            self.update_share_history_data(code)
+        return pd.read_csv(code+'.csv')
+ 
+    def has_share_history_local_data(self,code):
+        return os.path.exists(code+'.csv')
+
+    def update_share_history_data(self,code):
+        self._log("update data from internet")
+        data = ts.get_hist_data(code)
+        data.to_csv(code+".csv")
+
     def _log(self,str):
-        print("DataBase:"+str);
+        print("DataBase:"+str)
+
     def __init__(self):
         self._log("---init---")
 
 if __name__ == "__main__":
     dataBase = DataBase()
+    data = dataBase.get_share_history_data('600845')
+    print(data)
