7d34cc0e3094cb0deb7e2f0b850acdced96190e5
jianghao <256481788@163.com>
2016-05-19 19:34:56 +0800

    add update_all_share_history_data function

--- DataBaseModule.py
+++ DataBaseModule.py
@@ -3,12 +3,20 @@ import os
 import pandas as pd
 
 class DataBase:
+    def update_all_share_history_data(self):
+        self._log("update all share history data");
+        self.update_share_list()
+        data = self.get_share_list()
+        dataCodes = data.code
+        for code in dataCodes:
+            self.update_share_history_data(str(code))
+
     def update_share_list(self):
         self._log("updata share list form internet")
         data = ts.get_stock_basics()
         data.to_csv("share_list.csv")
 
-    def has_share_list_local(self);
+    def has_share_list_local(self):
         return os.path.exists("share_list.csv")
 
     def get_share_list(self):
@@ -25,7 +33,7 @@ class DataBase:
         return os.path.exists(code+'.csv')
 
     def update_share_history_data(self,code):
-        self._log("update data from internet")
+        self._log("update data from interneti code="+code)
         data = ts.get_hist_data(code)
         data.to_csv(code+".csv")
 
@@ -37,5 +45,6 @@ class DataBase:
 
 if __name__ == "__main__":
     dataBase = DataBase()
-    data = dataBase.get_share_list()
-    print(data)
+    dataBase.update_all_share_history_data()
+    #data = dataBase.get_share_list()
+    #print(data)
