a6233d9436e221f56566b7ce5c45fb6ff93e58b6
jianghao <2564811788@163.com>
2017-05-20 07:28:08 +0800

    add comments and change function names

--- DataBaseModule.py
+++ DataBaseModule.py
@@ -10,23 +10,32 @@ class DataBase:
     updateAllShareHistoryDataCount = 0
     indexNameMap = {'hs300':399300}
     
+    """
+    获得沪深300成份股的列表，以权重降序排序
+    """
     def get_hs300_info(self):
         data = ts.get_hs300s()
         if isinstance(data,pd.DataFrame):
             return data.sort_values(by='weight',ascending=False)
         else:
             return None
-
-    def update_share_history_data_by_codes(self,codes):
+        
+    """
+    获取codes列表指明的股票数据
+    """
+    def __update_share_history_data_by_codes(self,codes):
         for code in codes:
             self.updateAllShareHistoryDataCount += 1
             self.update_share_history_data(code)
-            self._log("finish "+str(self.updateAllShareHistoryDataCount)+"/"+str(self.updateAllShareHistoryDataSum))
+            self.__log("finish "+str(self.updateAllShareHistoryDataCount)+"/"+str(self.updateAllShareHistoryDataSum))
 
+    """
+    更新所有的股票信息
+    """
     def update_all_share_history_data(self):
-        self._log("update all share history data");
-        self.update_share_list()
-        data = self.get_share_list()
+        self.__log("update all share history data");
+        self.__get_share_list_from_internet()
+        data = self.__get_share_list()
         dataCodes = data.code
         for key in self.indexNameMap:
             #print(self.indexNameMap[key])
@@ -35,60 +44,68 @@ class DataBase:
         self.updateAllShareHistoryDataSum = len(dataCodes)
         data1 = dataCodes[0:int(self.updateAllShareHistoryDataSum/2)]
         data2 = dataCodes[int(self.updateAllShareHistoryDataSum/2):]
-        threading.Thread(target=self.update_share_history_data_by_codes,args=([data1])).start()
-        threading.Thread(target=self.update_share_history_data_by_codes,args=([data2])).start()
+        threading.Thread(target=self.__update_share_history_data_by_codes,args=([data1])).start()
+        threading.Thread(target=self.__update_share_history_data_by_codes,args=([data2])).start()
         #self.update_share_history_data_by_codes(dataCodes)
 
-    def update_share_list(self):
-        self._log("updata share list form internet")
+    def __get_share_list_from_internet(self):
+        self.__log("updata share list form internet")
         data = ts.get_stock_basics()
         data.to_csv("share_list.csv",encoding='utf-8')
 
-    def has_share_list_local(self):
+    def __has_share_list_local(self):
         return os.path.exists("share_list.csv")
 
-    def get_share_list(self):
-        if not self.has_share_list_local():
-            self.update_share_list()
-        return pd.read_csv("share_list.csv")
+    def __get_share_list(self):
+        if self.__has_share_list_local():
+            return pd.read_csv("share_list.csv")
+        else:
+            self.__log("can not find share_list.csv")
+            return None
 
+    """
+    获取个股信息，如果有本地数据，先拿本地数据,如果没有本地数据，先更新，再拿本地数据
+    """
     def get_share_history_data(self,code):
-        if not self.has_share_history_local_data(code):
+        if not self.__has_share_history_local_data(code):
             self.update_share_history_data(code)
-        if self.has_share_history_local_data(code):
-            return pd.read_csv(self._makeLocalShareDataPath(code))
+        if self.__has_share_history_local_data(code):
+            return pd.read_csv(self.__makeLocalShareDataPath(code))
         else:
             return None
  
-    def has_share_history_local_data(self,codestr):
-        code = self._formtInputCode(codestr)
-        return os.path.exists(self._makeLocalShareDataPath(code))
+    def __has_share_history_local_data(self,codestr):
+        code = self.__formtInputCode(codestr)
+        return os.path.exists(self.__makeLocalShareDataPath(code))
 
+    """
+    更新个股信息
+    """
     def update_share_history_data(self,codestr):
-        code = self._formtInputCode(codestr)
-        self._log("update data from internet code="+code)
+        code = self.__formtInputCode(codestr)
+        self.__log("update data from internet code="+code)
         data = ts.get_hist_data(code)
         if isinstance(data,pd.DataFrame):
-            data.to_csv(self._makeLocalShareDataPath(code),encoding='utf-8')
+            data.to_csv(self.__makeLocalShareDataPath(code),encoding='utf-8')
         else:
-            self._log("update data from internet code="+code+" but not get data")
+            self.__log("update data from internet code="+code+" but not get data")
 
-    def _isInIndexNameMap(self,name):
+    def __isInIndexNameMap(self,name):
         if name in indexNameMap.keys:
             return True
         return False
 
-    def _getIndexCode(self,name):
+    def __getIndexCode(self,name):
         return indexNameMap[name]
 
-    def _makeLocalShareDataPath(self,code):
+    def __makeLocalShareDataPath(self,code):
         timestr = ''#time.strftime('%Y-%m-%d', time.localtime())
-        codestr = self._formtInputCode(code)
+        codestr = self.__formtInputCode(code)
         if not os.path.exists("./localShareData/"):
             os.mkdir("./localShareData/")
         return "./localShareData/"+codestr+"_"+timestr+".csv"
         
-    def _formtInputCode(self,code):
+    def __formtInputCode(self,code):
         codestr = str(code)
         dlen = 6-len(codestr)
         while(dlen > 0):
@@ -97,17 +114,17 @@ class DataBase:
         return codestr
             
     
-    def _log(self,str):
+    def __log(self,str):
         print("DataBase:"+str)
 
     def __init__(self):
-        self._log("---init---")
+        self.__log("---init---")
 
 if __name__ == "__main__":
     dataBase = DataBase()
     #dataBase._makeLocalShareDataPath(100)
     #dataBase.get_share_history_data(300512)
-    #dataBase.update_all_share_history_data()
+    dataBase.update_all_share_history_data()
     #data = ts.get_hist_data('399300')
     #print(data)
-    print(dataBase.get_hs300_info())
+    #print(dataBase.get_hs300_info())
