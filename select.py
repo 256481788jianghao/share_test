84ee3d9b7a6d8828433fbff17cad26c8cc2c09f1
jianghao <2564811788@163.com>
2017-07-14 10:09:41 +0800

    change set moth

--- select.py
+++ select.py
@@ -31,10 +31,20 @@ def getData(code=None,startDate=None,endDate=None,ft=None):
             #print(data)
     #print(len(data_list))    
     frame = pd.concat(data_list)#,ignore_index=True)
-    frame['code'] = code_list
-    frame['turnover_rate'] = turnover_rate_list
+    frame.loc[:,'code'] = code_list
+    frame.loc[:,'turnover_rate'] = turnover_rate_list
     return frame
 
 #data = getData('300024','2017-07-04')
-data = getData(None,'2017-07-05','2017-07-04')
-print(data)
+def FT(data):
+    if len(data) == 3:
+        return True
+    else:
+        return False
+data = getData(None,'2017-07-05','2017-07-03',ft=FT)
+data2 = data.groupby('code').apply(lambda x:x.p_change)
+
+k1 = data2[(data2['2017-07-04'] > 0) & (data2['2017-07-03']>8)]
+k2 = data2[data2['2017-07-03']>8]
+
+print(len(k1)/len(k2))
